ee0f67f3efc8545e8543c4cb589e68c2b1f766b4
Dan Amemba <danielamemba@gmail.com>
2022-06-03 06:49:42 -0400

    added git actions file

--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import Car, Review
+from .models import Car
 from .utils import average_rating
 
 
@@ -15,15 +15,12 @@ def car_list(request):
         # get total reviews
         reviews = car.review_set.all()
         if reviews:
-            car_rating = average_rating([review.rating for \
-                                         review in reviews])
+            car_rating = average_rating([review.rating for review in reviews])
             number_of_reviews = len(reviews)
         else:
             car_rating = None
             number_of_reviews = 0
-        all_cars.append({'car': car, \
-                         'car_rating': car_rating, \
-                         'number_of_reviews': number_of_reviews})
+        all_cars.append({'car': car, 'car_rating': car_rating, 'number_of_reviews': number_of_reviews})
         context = {
             'car_list': all_cars}
     return render(request, 'store_templates/car_list.html', context)
@@ -34,13 +31,11 @@ def car_list(request):
 
 def car_detail(request, pk):
     car_det = get_object_or_404(Car, pk=pk)
-    car_reviews = []
     # get total reviews
     reviews = car_det.review_set.all()
     # declare the object's values in context
     if reviews:
-        car_rating = average_rating([review.rating for \
-                                     review in reviews])
+        car_rating = average_rating([review.rating for review in reviews])
         context = {
             'car': car_det,
             'car_rating': car_rating,
